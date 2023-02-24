754949abc6a459d6268a69d94542dd213789407d
AparicioFermiano <aparicio.empresarial@gmail.com>
2023-02-23 21:18:00 -0300

    transformacao em classe

--- post_shortpixel.py
+++ post_shortpixel.py
@@ -1,31 +1,37 @@
 import requests
 import json
 
-# Sua chave individual da ShortPixel
-key = '<sua chave shortpixel>'
+class ShortPixelAPI:
+    def __init__(self, key):
+        # Sua chave individual da ShortPixel
+        self.key = key
+        # Url formatada com a chave
+        self.url_format = 'https://api.shortpixel.com/v2/post-reducer.php?key=%s' % key
 
-# Caminho da imagem
-imagem = '<caminho da sua imagem>'
+    def upload_imagem(self, imagem_path, nome_imagem):
+        data = {
+            'key': self.key, 
+            'lossy': 0, 
+            'file_paths': json.dumps({nome_imagem: imagem_path}),
+            'wait': 30
+        }
+        with open(imagem_path, 'rb') as f:
+            files = {nome_imagem: f}
+            r = requests.post(
+                url=self.url_format,
+                data=data,
+                files=files
+            )
+        return r
 
-# Nome especifico, utilizado no relatorio
-nome_imagem = 'file1'
-
-# Url formatada com a chave
-url_format = 'https://api.shortpixel.com/v2/post-reducer.php?key=%s' % key
-
-# Dados enviados pela API com os parametros da documentacao
-data = {
-   'key': key, 
-   'lossy': 0, 
-   'file_paths': json.dumps({nome_imagem: imagem}),
-   'wait': 30
-}
-
-# Request
-r = requests.post(
-    url=url_format,
-    files={nome_imagem: open(imagem, 'rb')},
-    data=data)
-
-# Exibicao
-print(r.json())
+    def reduzir_tamanho(self, nome_imagem):
+        data = {
+            'key': self.key, 
+            'lossy': 1, 
+            'file_names': json.dumps([nome_imagem])
+        }
+        r = requests.post(
+            url=self.url_format,
+            data=data
+        )
+        return r
